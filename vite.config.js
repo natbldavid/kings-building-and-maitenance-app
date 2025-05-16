1c48a88ad9ce7e53436c9c85158678b84e0b3890
natbldavid <natbldavid@gmailcom>
2025-05-16 14:57:21 +0100

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/kings-building-and-maitenance-app", // This is important for Netlify deployments
-});
